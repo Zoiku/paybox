72ef10c12619ead1b8a7e7ce2ad57d8b1c637087
kwabena koranteng <kwabena.obeng@ashesi.edu.gh>
2021-04-20 00:24:19 +0000

    Updated payment

--- src/app/payment/payment.page.ts
+++ src/app/payment/payment.page.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
 import { CartService } from '../services/cart.service';
 import { HTTP } from '@ionic-native/http/ngx';
-
+import {AlertController, NavController} from '@ionic/angular';
+import{Router,NavigationExtras} from '@angular/router';
 @Component({
   selector: 'app-payment',
   templateUrl: './payment.page.html',
@@ -16,8 +17,10 @@ export class PaymentPage implements OnInit {
   mode:string = '';
   mobile_network:string = '';
   mobile_number:string = '';
+  id:any;
+  responseData:any={};
 
-  constructor(private modalCtrl:ModalController, private cart: CartService, private http:HTTP ) { }
+  constructor(private modalCtrl:ModalController,private router: Router, private cart: CartService, private http:HTTP,private alertctrl:AlertController ) { }
 
   async close() {
     await this.modalCtrl.dismiss();
@@ -30,9 +33,44 @@ export class PaymentPage implements OnInit {
     try {
       let msg = await this.http.post(`https://www.paybox.com.co/pay?amount=${this.amount}&currency=${this.currency}&mode=${this.mode}&mobile_network=${this.mobile_network}&mobile_number=${this.mobile_number}&voucher_code=1234&payload={}&order_id=PB_12345`, {}, {
         Authorization: 'Bearer 905ea86a-1935-4e4d-9718-7f3e0a774d72'
+      }).then(data=>{
+        this.responseData=JSON.parse(data.data);
       })
-    } catch (error) {
-      console.log(error);
-    }
+      this.id=this.responseData.token;
+
+      if(this.id =="undefined"){
+        await this.alertctrl.create({
+          header:'Failed',
+          subHeader:'Transaction failed. Please try again later',
+          buttons:[{
+            text:"Okay",
+            handler:()=>{
+              this.router.navigate(['/payment']);
+            }
+          }]
+        }).then((confirmElement)=>{
+          confirmElement.present()
+        })
+      }
+      else{
+        await this.alertctrl.create({
+            header:'Success',
+            subHeader:'Transaction was successful',
+            buttons:[{
+              text:"Okay",
+              handler:()=>{
+                this.router.navigate(['/home']);
+              }
+            }]
+          }).then((confirmElement)=>{
+            confirmElement.present()
+          })
+  
+        }
+      }
+      catch (error) {
+        console.log(error);
+      }
+    } 
   }
-}
+
